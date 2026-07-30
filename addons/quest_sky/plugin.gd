7f66d339c9b3082e7dbc6322c7c461603939ec89
Leon <45938585+sn-Cloud@users.noreply.github.com>
2026-07-30 22:40:43 +0800

    Remove duplicate native export registration

--- addons/quest_sky/plugin.gd
+++ addons/quest_sky/plugin.gd
@@ -1,27 +1,8 @@
 @tool
 extends EditorPlugin
 
-var _export_plugin: QuestSkyExportPlugin
-
 func _enter_tree() -> void:
-    _export_plugin = QuestSkyExportPlugin.new()
-    add_export_plugin(_export_plugin)
+    pass
 
 func _exit_tree() -> void:
-    if _export_plugin != null:
-        remove_export_plugin(_export_plugin)
-        _export_plugin = null
-
-class QuestSkyExportPlugin:
-    extends EditorExportPlugin
-
-    func _get_name() -> String:
-        return "QuestSkyNativeExport"
-
-    func _export_begin(features: PackedStringArray, _is_debug: bool, _path: String, _flags: int) -> void:
-        if features.has("android"):
-            add_shared_object(
-                "res://godot/bin/android/arm64-v8a/libquest_sky_native.so",
-                PackedStringArray(["arm64"]),
-                ""
-            )
+    pass
