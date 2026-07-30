e368accd75b7462542f9f361f067c41b616fe33f
Leon <45938585+sn-Cloud@users.noreply.github.com>
2026-07-30 22:28:40 +0800

    Explicitly export Android ARM64 native library

--- addons/quest_sky/plugin.gd
+++ addons/quest_sky/plugin.gd
@@ -1,8 +1,27 @@
 @tool
 extends EditorPlugin
 
+var _export_plugin: QuestSkyExportPlugin
+
 func _enter_tree() -> void:
-    pass
+    _export_plugin = QuestSkyExportPlugin.new()
+    add_export_plugin(_export_plugin)
 
 func _exit_tree() -> void:
-    pass
+    if _export_plugin != null:
+        remove_export_plugin(_export_plugin)
+        _export_plugin = null
+
+class QuestSkyExportPlugin:
+    extends EditorExportPlugin
+
+    func _get_name() -> String:
+        return "QuestSkyNativeExport"
+
+    func _export_begin(features: PackedStringArray, _is_debug: bool, _path: String, _flags: int) -> void:
+        if features.has("android"):
+            add_shared_object(
+                "res://godot/bin/android/arm64-v8a/libquest_sky_native.so",
+                PackedStringArray(["arm64"]),
+                ""
+            )
